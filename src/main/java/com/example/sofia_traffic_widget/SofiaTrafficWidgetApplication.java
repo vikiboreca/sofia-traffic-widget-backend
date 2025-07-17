29d0d304a47c90b6614b3a33000bf768bce1bc8c
vikiboreca <vikiboreca@gmail.com>
2025-07-17 15:11:11 +0300

    commit

---
+++ src/main/java/com/example/sofia_traffic_widget/SofiaTrafficWidgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofia_traffic_widget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofiaTrafficWidgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofiaTrafficWidgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sofia_traffic_widget/SofiaTrafficWidgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofia_traffic_widget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiaTrafficWidgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
